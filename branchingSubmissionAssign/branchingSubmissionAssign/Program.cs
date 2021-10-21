10c8bb9f5babc8749e3c59f43ea7124a31487041
Gabe Larson <gabriel.g.larson@gmail.com>
2021-10-21 16:06:44 -0700

    .

---
+++ branchingSubmissionAssign/branchingSubmissionAssign/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace branchingSubmissionAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //getting an input, converting and saving it as an int
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.\nPlease enter the package weight:");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            //If the package weight is greater than 50 this message will be shown and the program will end there. If not the program will continue on
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                //getting an input, converting and saving it as an int
+                Console.WriteLine("Please enter the package width:");
+                int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+                //getting an input, converting and saving it as an int
+                Console.WriteLine("Please enter the package height:");
+                int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+                //getting an input, converting and saving it as an int
+                Console.WriteLine("Please enter the package length:");
+                int packageLength = Convert.ToInt32(Console.ReadLine());
+
+                //calculating the quote price for the package by multiplying ((height x width x length) x (weight)) and dividing by 100
+                //each measurement has to first be converted into a decimal for the final answer to have two decimal places as money should
+                decimal quote = ((Convert.ToDecimal(packageHeight) * Convert.ToDecimal(packageWidth) * Convert.ToDecimal(packageLength)) * Convert.ToDecimal(packageWeight)) / 100;
+
+                //Displays the users quote
+                Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ practicingSideChallenges/practicingSideChallenges/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicingSideChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 22;
+
+            if (age > 30)
+            { 
+                Console.WriteLine("Older than 30");
+            }
+            else if (age < 20)
+            {
+                Console.WriteLine("Younnger than 20");
+            }
+            else if (age != 22)
+            {
+                Console.WriteLine("You're not 22"); 
+            }
+            else
+            {
+                Console.WriteLine("I'm sure you're some age");
+                Console.ReadLine();
+            }
+
+            //Example of a Ternary operator..represented by the symbol "?"
+            int num1 = 7;
+            int num2 = 12;
+            //If it is true the message on the left of the ":" will print, if false the right side will print
+            string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+            Console.WriteLine(result);
+        }
+    }
+}
