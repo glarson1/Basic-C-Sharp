ae90b39d34f31dd5b464a9cff1ab280c670ff2f7
Gabe Larson <gabriel.g.larson@gmail.com>
2021-10-21 14:19:52 -0700

    added comments

--- BooleanLogicSubmission/BooleanLogicSubmission/Program.cs
+++ BooleanLogicSubmission/BooleanLogicSubmission/Program.cs
@@ -10,15 +10,19 @@ namespace BooleanLogicSubmission
     {
         static void Main(string[] args)
         {
+            //Getting user input, converting and saving as an int
             Console.WriteLine("What is your age? (answer with integer. ex: 43)");
             int age = Convert.ToInt32(Console.ReadLine());
 
+            //Getting user input, converting and saving as an int
             Console.WriteLine("Have you ever had a DUI? (answer with true or false)");
             bool dui = Convert.ToBoolean(Console.ReadLine());
 
+            //Getting user input, converting and saving as an int
             Console.WriteLine("How many speeding tickets do you have? (answer with integer. ex: 3)");
             int tickets = Convert.ToInt32(Console.ReadLine());
 
+            //Checking if input meets requirements. Will print a true or false if user meets requirements
             Console.WriteLine("Qualified?");
             Console.WriteLine(age > 15 && dui == false && tickets <= 3);
             Console.ReadLine();
