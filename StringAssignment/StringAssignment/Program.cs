6e5d82193c15cafe3e7c575714dc456db5a2b533
Gabe Larson <gabriel.g.larson@gmail.com>
2021-10-23 14:03:29 -0700

    .

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "One";
+            string two = "Two";
+            string three = "Three";
+
+            string count = one + two + three;
+
+            string name = "Gabe";
+
+            name = name.ToUpper();
+
+            StringBuilder yuh = new StringBuilder("Hello ");
+            //Append puts it on the same line. AppendLine puts it on a new line
+            yuh.Append("World");
+            yuh.AppendLine("\nlskdjflksjflkdsjflkdsjkfjksljfsd");
+            yuh.AppendLine("kkkkkkkkkkkkkkk");
+            yuh.AppendLine("ssssssss");
+
+
+            Console.WriteLine(yuh);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jesse";
+            string quote = "The man said, \"Hello, Jesse.\"";
+
+            bool trueOrFalse = name.Contains("s");
+            trueOrFalse = name.EndsWith("e");
+
+            int length = name.Length;
+
+            name = name.ToUpper();
+
+            name = name.ToLower();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+            
+        }
+    }
+}
