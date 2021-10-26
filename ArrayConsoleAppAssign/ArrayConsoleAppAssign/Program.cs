afdcf1a474333b6d8c3cca86c8ef055e45bc827e
Gabe Larson <gabriel.g.larson@gmail.com>
2021-10-26 10:11:06 -0700

    .

---
+++ ArrayConsoleAppAssign/ArrayConsoleAppAssign/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayConsoleAppAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //created an array of strings
+            String[] stringArray = { "apple", "baseball", "jumping", "spider", "yellow" };
+
+            //getting user input and saving it as an int
+            Console.WriteLine("Select an index of the array you can't see yet to display the value at that index. (Ex: 2)");
+            int userIndex = Convert.ToInt32(Console.ReadLine());
+
+            //displaying the users index on the screen
+            Console.WriteLine(stringArray[userIndex]);
+            Console.ReadLine();
+
+            //created an array of integers
+            int[] numArray = { 21, 33, 44, 55, 66, 333 };
+
+            //getting user input and saving it as an int
+            Console.WriteLine("Select an index of the array you can't see yet to display the value at that index. (Ex: 2)");
+            int userIndex2 = Convert.ToInt32(Console.ReadLine());
+
+            //adding this to allow a message if user selects an index that doesn't exist
+            if (userIndex2 > 5)
+            {
+                Console.WriteLine("That index doesn't exist");
+                Console.ReadLine();
+            }
+            else
+            {
+                //displaying the users index on the screen
+                Console.WriteLine(numArray[userIndex]);
+                Console.ReadLine();
+            }
+
+
+            //Created a list of strings
+            List<string> stringList = new List<string>{ "bear", "cat", "balloon", "car" };
+
+            //getting user input and saving it as an int
+            Console.WriteLine("Select an index of the list you can't see yet to display the value at that index. (Ex: 2)");
+            int userIndex3 = Convert.ToInt32(Console.ReadLine());
+
+            //displaying the index
+            Console.WriteLine(stringList[userIndex]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates an array saying it will have a length of 5. cannot leave blank
+            int[] numArray = new int[5];
+            numArray[0] = 5;
+            numArray[1] = 2;
+            numArray[2] = 10;
+            numArray[3] = 200;
+            numArray[4] = 5000;
+
+
+            //easier example making a different array
+            int[] numArray1 = new int[] { 5, 4, 33, 44, 44434 };
+
+            //an even easier way than the easy way
+            int[] numArray2 = { 3, 4, 5, 44, 3443 };
+
+            //say i wanted to change the 5 to a 7
+            numArray2[2] = 7;
+
+            Console.WriteLine(numArray[3]);
+            Console.ReadLine();
+
+            //creating a list now....can also do List<string> or any other data type
+            List<int> intList = new List<int>();
+            intList.Add(4);
+            intList.Add(10);
+            intList.Remove(10);
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+        }
+    }
+}
