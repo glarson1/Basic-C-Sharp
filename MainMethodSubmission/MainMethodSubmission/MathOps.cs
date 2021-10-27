669a3f5457803a22c782c18b58e2884711ee5894
Gabe Larson <gabriel.g.larson@gmail.com>
2021-10-27 11:29:59 -0700

    finished assignment

---
+++ MainMethodSubmission/MainMethodSubmission/MathOps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    public class MathOps
+    {
+        //made all public so main() can call upon them
+        //This is the method that takes an int and returns an int
+        public void myMethod (int i)
+        {
+            int theThree = 3;
+            Console.WriteLine("Your number plus three is " + (i + theThree));
+        }
+        //This is the method that takes a decimal and returns an int
+        public void myMethod (decimal i)
+        {
+            int theFour = 4;
+            Console.WriteLine("Your decimal minus 4 equals the int " + ((Convert.ToInt32(i)) - theFour));
+        }
+        //This is the method that takes a string and returns an int
+        public void myMethod (string i)
+        {
+            int theFive = 5;
+            Console.WriteLine("Your string times 5 equals the int " + ((Convert.ToInt32(i)) * theFive));
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciating the class MathOps which will allow me to use the variable mathOps from here on out
+            MathOps mathOps = new MathOps();
+
+            //getting user input and making sure it is saved as an int
+            Console.WriteLine("Enter in a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            //calling the myMethod. It should tell by the data type what I am trying to do
+            mathOps.myMethod(userInput);
+
+         
+            //getting user input and making sure it is saved as a decimal
+            Console.WriteLine("Enter in a decimal:");
+            decimal userNum = Convert.ToDecimal(Console.ReadLine());
+            //calling the myMethod. It should tell by the data type what I am trying to do
+            mathOps.myMethod(userNum);
+
+       
+            //getting user input and making sure it is saved as a string
+            Console.WriteLine("Enter a number (it will begin in string form)");
+            string userString = Console.ReadLine();
+            //calling the myMethod. It should tell by the data type what I am trying to do
+            mathOps.myMethod(userString);
+
+            Console.ReadLine();
+        }
+    }
+}
