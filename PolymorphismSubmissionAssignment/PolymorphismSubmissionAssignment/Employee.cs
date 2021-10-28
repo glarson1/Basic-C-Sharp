1d01b02d420d032d9f3c86de40179ff36a39fd9c
Gabe Larson <gabriel.g.larson@gmail.com>
2021-10-28 12:31:12 -0700

    .

---
+++ PolymorphismSubmissionAssignment/PolymorphismSubmissionAssignment/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubmissionAssignment
+{
+    //Created a class named Employee that will inherit from the Person Class
+    public class Employee : Person, IQuittable
+    {    //impliments the SayName() from the Person class but overrides and gives it functionality to be used in main()
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+        //inherited quit method from IQuittable
+        public void Quit(Person person)
+        {
+            Console.WriteLine("This program will soon end");
+        }
+    }
+}
---
+++ PolymorphismSubmissionAssignment/PolymorphismSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling in the Employee class
+            Employee employee = new Employee();
+
+            //give values to the properties
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            //call sayName() which will use the property values to do its job and display to the console
+            employee.SayName();
+            Console.ReadLine();
+
+            employee.Quit(employee);
+            Console.ReadLine();
+        }
+    }
+}
