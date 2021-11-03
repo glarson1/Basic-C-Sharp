5c9a6b0115307c80233046133b28661b58502800
Gabe Larson <gabriel.g.larson@gmail.com>
2021-11-03 12:57:50 -0700

    .

---
+++ ConstructorSubmission/ConstructorSubmission/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    //created two constructors in this class. This will assign a value to a variable that will be displayed on program.cs
+    public class MyClass
+    {
+        public string nameOfSchool;
+        public MyClass() : this("The Tech Academy")
+        {
+
+        }
+
+        public MyClass(string schoolName)
+        {
+            nameOfSchool = schoolName;
+        }
+    }
+
+    
+}
---
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a const variable
+            const int G = 44;
+
+            //create a variable using "var"
+            var lizard = "reptile";
+
+            //This showing to the console shows that I chained two constructors together
+            MyClass mc = new MyClass();
+            Console.WriteLine(mc.nameOfSchool);
+            Console.ReadLine();
+
+        }
+    }
+}
