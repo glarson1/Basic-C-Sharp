92be7e4ac3bbe4678130b12db383a2f9293b0c29
Gabe Larson <gabriel.g.larson@gmail.com>
2021-11-07 12:54:37 -0800

    .

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for their age
+            Console.WriteLine("How many years old are you?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            DateTime datetime = DateTime.Now;
+
+            DateTime yearBorn = new datetime.AddYears(-userAge));
+
+            Console.ReadLine();
+
+            //Display th
+        }
+    }
+}
